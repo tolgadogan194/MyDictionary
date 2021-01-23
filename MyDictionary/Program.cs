c3e205656f193d63043f45868d0c19b3e3e297b8
TOLGA DOGAN <tdogan194@posta.pau.edu.tr>
2021-01-23 17:01:59 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> ar = new List<string>();
+            MyList<string> ars = new MyList<string>();
+            ars.Add("tt");
+            foreach (string arss in ars)
+            {
+                Console.WriteLine(arss);
+            }
+            
+        }
+    }
+}
